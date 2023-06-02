2fbd84ca5da673067de0b7a85dda5cf167ba330b
unknown <eginer06@gmail.com>
2023-06-02 15:05:46 +0200

    primera subida

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'containerbackground' : '#ffdab9',
+      'white' : '#fff',
+      'membergrid' : '#d87093',
+      'body' : '#73edff'
+      },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
